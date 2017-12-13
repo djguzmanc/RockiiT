5e8378b69116223007a98752a145c6598efc0e0a
David <djguzmanc@unal.edu.co>
2017-12-13 17:21:59 -0500

    toolbar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/landing/footer/footer.component';
+import { TimelineComponent } from './components/landing/timeline/timeline.component';
 
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    TimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
