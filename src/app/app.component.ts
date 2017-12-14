9e5d4e8e89e734f1450e18f77f031e7b988ae190
David <djguzmanc@unal.edu.co>
2017-12-14 07:20:46 -0500

    All changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var firebase: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit( ) {
+    firebase.auth().onAuthStateChanged( user => {
+        if ( user ) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+
+          console.log( "user is in" )
+          localStorage.setItem( 'userIsIn', 'true' )
+        } else {
+          localStorage.setItem( 'userIsIn', 'false' )
+          console.log( "user is out" )
+        }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,30 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 
+import { MatDialogModule, MatInputModule, MatButtonModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { FooterComponent } from './components/landing/footer/footer.component';
 import { TimelineComponent } from './components/landing/timeline/timeline.component';
 
+import { HorizontalTimelineComponent } from './components/horizontal-timeline/horizontal-timeline.component';
+import { SignUpComponent } from './components/landing/sign-up/sign-up.component';
+import { SignInComponent } from './components/landing/sign-in/sign-in.component';
+import { LandingComponent } from './components/landing/landing/landing.component';
+import { HistoryWallComponent } from './components/history-wall/history-wall.component';
+import { PostExpComponent } from './components/landing/post-exp/post-exp.component';
+import { PostInfoComponent } from './components/post-info/post-info.component';
+
 export const appRoutes: Routes = [
   {
     path: '',
-    component: AppComponent
+    component: LandingComponent
   },
+  {
+    path: 'history/:year',
+    component: HistoryWallComponent
+  }
 ];
 
 @NgModule({
@@ -21,13 +35,30 @@ export const appRoutes: Routes = [
     AppComponent,
     ToolbarComponent,
     FooterComponent,
-    TimelineComponent
+    TimelineComponent,
+    HorizontalTimelineComponent,
+    SignUpComponent,
+    SignInComponent,
+    LandingComponent,
+    HistoryWallComponent,
+    PostExpComponent,
+    PostInfoComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule.forRoot( appRoutes )
   ],
-  providers: [],
+
+  entryComponents: [
+    SignUpComponent,
+    SignInComponent,
+    PostExpComponent,
+    PostInfoComponent
+  ],
   bootstrap: [AppComponent]
 })
 
